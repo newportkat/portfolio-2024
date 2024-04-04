102a6465bd61ff91ff8c42d39ba0d5fcd6befdb2
newportkat <thomas.katilius@gmail.com>
2024-04-04 19:48:24 +1100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-full min-h-screen bg-gray-900">
+      <h1 className="text-gray-200 text-7xl font-pixel">Tom Katilius</h1>
+      <h2 className="text-xl text-gray-200 font-roboto">
+        Full-Stack Developer
+      </h2>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pixel: ['"Press Start 2P"', "ui-monospace", "monospace"],
+        roboto: ['"Roboto Mono"', "ui-monospace", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+}
